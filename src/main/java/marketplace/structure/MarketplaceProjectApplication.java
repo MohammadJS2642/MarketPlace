7c58bd95f2e2b3484c6bad61249776d154eb988c
MohammadJS2642 <mohammadjs2642@gmail.com>
2023-03-03 21:57:33 +0330

    marketplace project initialized

---
+++ src/main/java/marketplace/structure/MarketplaceProjectApplication.java
@@ -0,0 +1,13 @@
+package marketplace.structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marketplace/structure/MarketplaceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package marketplace.structure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
